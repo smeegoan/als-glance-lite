47ca165cd274cb678881829af9e26032d16d7d60
Pedro Cardoso <smeegoan@gmail.com>
2015-11-03 23:28:19 +0000

    Removed unneeded scripts

--- ALS.Glance.SqlDatabase/dbo/Tables/D_Time.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[D_Time] (
-    [Id]        BIGINT             IDENTITY (1, 1) NOT NULL,
-    [Hour]      SMALLINT           NOT NULL,
-    [TimeOfDay] NVARCHAR (50)      NOT NULL,
-    [CreatedOn] DATETIMEOFFSET (7) DEFAULT (getdate()) NOT NULL,
-    [UpdatedOn] DATETIMEOFFSET (7) DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [PK_dbo.Time] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
